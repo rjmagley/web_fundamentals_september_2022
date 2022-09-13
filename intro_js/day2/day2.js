2e73f96c6f1f07ed34607dae545e716a28afa0da
Ryan Magley <ryan.magley@gmail.com>
2022-09-13 16:25:58 -0500

    09/13 push

---
+++ intro_js/day2/day2.js
@@ -0,0 +1,13 @@
+// javascript
+// frontend - frontend functionality, robust
+// java and javascript?
+
+// variables
+// referencing data (storing data)
+
+var x = 3;
+// we can also use the keywords let and const
+console.log(x);
+x = x + 7;
+
+console.log(x);
---
+++ intro_js/day2/day2x.js
@@ -0,0 +1,8 @@
+// loops
+
+// for loop to print the numbers 1 through 1900
+// print all the multiples of 10 from 1 to 1900
+
+for (var i = 10; i <= 1900; i = i + 10) {
+    console.log(i);
+}
